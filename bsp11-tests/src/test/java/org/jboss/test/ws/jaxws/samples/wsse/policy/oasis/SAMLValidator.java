a98c157174a85eb21aa9c104eeb53e1cff29f05f
rsvoboda <rsvoboda@redhat.com>
2014-11-19 13:50:36 +0000

    WSI BSP update for WSS-512 changes, details in BZ 1165622 and BZ 1153968

--- bsp11-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsse/policy/oasis/SAMLValidator.java
+++ bsp11-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsse/policy/oasis/SAMLValidator.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2014, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -21,31 +21,10 @@
  */
 package org.jboss.test.ws.jaxws.samples.wsse.policy.oasis;
 
-import javax.ejb.Stateless;
-import javax.jws.WebService;
+public class SAMLValidator extends org.apache.ws.security.validate.SamlAssertionValidator {
 
-import org.apache.cxf.annotations.EndpointProperties;
-import org.apache.cxf.annotations.EndpointProperty;
-import org.jboss.ws.api.annotation.WebContext;
-
-@WebService
-(
-   portName = "SecurityService2311Port",
-   serviceName = "SecurityService",
-   wsdlLocation = "WEB-INF/wsdl/SecurityService23x.wsdl",
-   targetNamespace = "http://www.jboss.org/jbossws/ws-extensions/wssecuritypolicy/oasis-samples",
-   endpointInterface = "org.jboss.test.ws.jaxws.samples.wsse.policy.oasis.ServiceIface"
-)
-@Stateless
-@EndpointProperties(value = {
-    @EndpointProperty(key = "ws-security.saml1.validator", value = "org.jboss.test.ws.jaxws.samples.wsse.policy.oasis.SAMLValidator")
+    public SAMLValidator() {
+        super();
+        setRequireBearerSignature(false);
     }
-)
-@WebContext(urlPattern = "SecurityService2311")
-public class Service2311Impl implements ServiceIface
-{
-   public String sayHello()
-   {
-      return "Hello - (WSS1.0) SAML1.1 Assertion (Bearer)";
-   }
 }
